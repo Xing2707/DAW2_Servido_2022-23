00d38601e8416b6934e1f44c3870a317631c2f5b
XingChen <xing5539@gmail.com>
2022-10-05 16:26:58 -0700

    ArrayFuncion

--- PHP_Tema_1/Practica_3/buscar_passw.php
+++ PHP_Tema_1/Practica_3/buscar_passw.php
@@ -1,22 +1,46 @@
 <?php
+    set_time_limit(0);
     $hash="$2y$10$0GNiidCkeO/VBBHPH0DP6e5tgz6l/FIOxs1RcFloJrXuTYmmAsW72";
     $acepdato=false;
     $num1=97;
     $num2=97;
     $num3=97;
     $num4=97;
-    for($i=0; $i<26; $i++){
-        for($y=0; $y<26; $y++){
-            for($c=0; $c<26; $c++){
-                for($a=0; $a<26; $a++){
-                    if($c++){
-                        $pass=chr($num1).chr($num2).chr($num3++).chr($num4);
-                    }else
-                        if($y++){
-                            $pass=chr($num1).chr($num2++).chr($num3).chr($num4);
-                        }
+    
+    for($i=0; $i<26 && !$acepdato; $i++){
+
+        for($y=0; $y<26 && !$acepdato; $y++){
+            
+            for($c=0; $c<26 && !$acepdato; $c++){
+              
+                for($a=0; $a<26 && !$acepdato; $a++){
+
                     $pass=chr($num1).chr($num2).chr($num3).chr($num4);
 
+                        if(password_verify($pass,$hash) || $i>122){
+                            $acepdato=true;
+                            print("la clave es: ".$pass);
+                            
+                        }else{
+                            if($num4>122){
+                                $num3+=1;
+                                $num4=97;
+                            }else{
+                                if($num3>122){
+                                    $num2+=1;
+                                    $num3=97;
+                                    $num4=97;
+                                }else{
+                                    if($num2>122){
+                                        $num1+=1;
+                                        $num2=97;
+                                        $num3=97;
+                                        $num4=97;
+                                    }
+                                }
+                            }
+                        }
+                    $num4+=1;
                 }
             }
         }
