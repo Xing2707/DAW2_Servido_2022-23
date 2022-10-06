c29a7cca5d4eceaca0c1bd7a79f62d374f63c1ba
XingChen <xing5539@gmail.com>
2022-10-06 16:09:43 -0700

    ArrayFuncion

--- PHP_Tema_1/Practica_3/Productos.php
+++ PHP_Tema_1/Practica_3/Productos.php
@@ -16,37 +16,20 @@ $productos = [
     ];
 
     for($i=0; $i<count($productos); $i++){
-        $cantidad[$i]=" ";
+        $cantidad[$i]=null;
     }
 
+
     function mostrar_producto($var){
-        
+        $nombre=array_keys($var);
         for($i=0; $i<count($var); $i++){
             print("<tr>");
-                print("<td>".key($var)."<td> <br> <td>" .$var[key($var)]. "</td> <br>");
-                print("<td> <input type='number' step='any' class=" .cant($i). " /> </td> <br>" );
+                print("<td>".$nombre[$i]."</td>  <td>" .$var[$nombre[$i]]. "</td> ");
+                print("<td> <input type='number' step='any' name='".$nombre[$i]."'/> </td>" );
             print("</tr>");
         }
     } 
     
-    function cant($var){
-        switch($var){
-            case 0: return print('nar');
-            case 1: return print('man');
-            case 2: return print('per');
-            case 3: return print('pla');
-            case 4: return print('kiw');
-            case 5: return print('mac');
-            case 6: return print('arr');
-            case 7: return print('sal');
-            case 8: return print('pat');
-            case 9: return print('pan');
-            case 10: return print('lec');
-            case 11: return print('piz');
-            case 12: return print('hel');
-
-        }
-    }
 
     
 
@@ -59,20 +42,69 @@ $productos = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Productos</title>
+    <style>
+        body{
+            margin: auto;
+            padding: 1%;
+        }
+        #fondo{
+            width: 80%;
+            margin: auto;
+        }
+        #fondo>h1{
+            text-align: center;
+            margin: auto;
+        }
+
+        #fondo>form{
+            width: 90%;
+            margin: auto;
+            padding: 0.5%;
+        }
+
+        #fondo>form>table{
+            width: 50%;
+        }
+        
+        #fondo>form>table,tr,td{
+            border-collapse: collapse;
+            border: 2px solid cyan;
+            margin: auto;
+            text-align: center;
+        }
+
+        #fondo>form>table>tr,td{
+            padding: 1%;
+        }
+
+        #boton{
+            width: 40%;
+            padding: 1%;
+        }
+        #total{
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
-        <div>
-            <table>
-                <tr>
-                    <td>Nombre</td>
-                    <td>Precio</td>
-                    <td>Cantidad</td>
-                </tr>
-                <?=mostrar_producto($productos)?>
-                <tr>
-                    
-                <tr>
-            </table>
+        <div id="fondo">
+            <h1>Lista de productos</h1>
+            <form action="Productos.php" method="get" >
+                <table>
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Precio</td>
+                        <td>Cantidad</td>
+                    </tr>
+                    <?=mostrar_producto($productos)?>
+                    <tr>
+                        <td colspan="3"><input type="submit" value="Enviar" id="boton"/></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" id="total">total:</td>
+                    <tr>
+                </table>
+            </form>
         </div>
 </body>
 </html>
