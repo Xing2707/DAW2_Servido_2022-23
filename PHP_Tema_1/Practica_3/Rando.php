f0ca3828809540855732f21017f46b07ed1bb8cd
juan <juanantonio@gmail.com>
2022-10-07 20:29:43 +0200

    ArrayFuncion

--- PHP_Tema_1/Practica_3/Rando.php
+++ PHP_Tema_1/Practica_3/Rando.php
@@ -1,3 +1,22 @@
 <?php
-    
+    $tarea=[
+        "Lavar Ropa",
+        "Cocinal",
+        "Barrir",
+        "Hacer Cama",
+        "Hacer Compra",
+    ];
+    $persona=[
+        "Papa",
+        "Mama",
+        "Hijo",
+        "Abuela",
+    ];
+
+    print("Array ante de aleatorio: <br>");
+    print_r($tarea);
+
+    $tarea2=array_rand($tarea,4);
+    print("<br> Array aleatorio: <br>");
+    print_r($tarea2);
 ?>
