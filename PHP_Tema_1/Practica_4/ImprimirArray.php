e01a6c7de0863ee32645d9f2143ec88af4a6ab47
juan <juanantonio@gmail.com>
2022-10-14 21:31:35 +0200

    Funciones

---
+++ PHP_Tema_1/Practica_4/ImprimirArray.php
@@ -0,0 +1,16 @@
+<?php
+    $usuario = [
+        "nombre" => "Pepe",
+        "dirección" => "Calle full número 14"
+        "teléfono" => "91 234 55 69",
+        "población" => "Madrid",
+        "edad" => 25,
+      ]
+    
+      function($item){
+        return
+      }
+    function format_form_user( array $valor){
+        
+    }
+?>
