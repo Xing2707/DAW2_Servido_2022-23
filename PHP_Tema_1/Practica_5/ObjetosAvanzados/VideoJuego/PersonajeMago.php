a6068f13382e8aacb4e4d158ce32b1ea7107eab7
XingChen <xing5539@gmail.com>
2022-10-27 15:07:31 -0700

    Objetos

--- PHP_Tema_1/Practica_5/ObjetosAvanzados/VideoJuego/PersonajeMago.php
+++ PHP_Tema_1/Practica_5/ObjetosAvanzados/VideoJuego/PersonajeMago.php
@@ -1,7 +1,6 @@
 <?php
     require_once("InterfazPersonaje.php");
     abstract class PersonajeMago implements AccionPersonaje{
-        use Posicion;
         public function defender():string { return "hechizo protector"; }
         abstract public function atacar():string ;
     }
