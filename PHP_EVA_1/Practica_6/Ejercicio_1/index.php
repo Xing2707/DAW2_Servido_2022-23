38c0aebd1e23c1b1bc88e696803ed470accee00b
juan <juanantonio@gmail.com>
2022-11-02 21:43:58 +0100

    Formularios

---
+++ PHP_EVA_1/Practica_6/Ejercicio_1/index.php
@@ -0,0 +1,48 @@
+<?php 
+    //$DireccionIP=$_SERVER['REMOTE_ADDR'];
+
+    $Ruta=$_SERVER['HTTP_USER_AGENT'];
+    $posiconInical=strpos($Ruta,';',0)+1;
+    $Equipo=substr($Ruta,$posiconInical,7);
+
+    $GetIdioma=$_SERVER['HTTP_ACCEPT_LANGUAGE'];
+    $PosicionIdioma=strpos($GetIdioma,'-',0);
+    $Idioma=substr($GetIdioma,0,$PosicionIdioma);
+
+    function imprimir($valo1,$valo2){
+        switch($valo2){
+            case "en":  
+                print("<h1>Welcome</h1>");
+                print("<p> Your visiting IP address is:".$_SERVER['REMOTE_ADDR']."</p>");
+                print("<p> Your Visiting Team is: ".$valo1."</p>");
+                print("<p> Your visiting language is: English");
+                break;
+            case "es":
+                print("<h1>Bien Venido</h1>");
+                print("<p> Tu direccion IP de visita es: ".$_SERVER['REMOTE_ADDR']."</p>");
+                print("<p> Tu Equipo de visita es: ".$valo1."</p>");
+                print("<p> Tu idioma de visita es: Español");
+                break;
+            case "zh":
+                print("<h1>欢迎</h1>");
+                print("<p> 您的访问IP地址是: ".$_SERVER['REMOTE_ADDR']."</p>");
+                print("<p> 您的访问计算机是: ".$valo1."</p>");
+                print("<p> 您的访问语言是:中文");
+                break;
+        }   
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?=imprimir($Equipo,$Idioma)?>
+    </div>
+</body>
+</html>
